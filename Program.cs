9486d663c44d8a046e464eaaf005434480298c47
ja-s-min <jasminhassani@outlook.com>
2025-01-06 17:41:02 +0100

    tionde commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using dagnys_api;
+using dagnys_api.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options =>{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DevConnection"));
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+var context = services.GetRequiredService<DataContext>();
+await context.Database.MigrateAsync();
+await Seed.LoadProducts(context);
+await Seed.LoadSupplier(context);
+await Seed.LoadRecipe(context);
+await Seed.LoadPurchase(context);
+await Seed.LoadRawMaterial(context);
+await Seed.LoadSupplierRawMaterial(context);
+}
+catch (Exception ex)
+{Console.WriteLine("{0}", ex.Message);
+throw;
+}
+
+app.MapControllers();
+
+app.Run();
